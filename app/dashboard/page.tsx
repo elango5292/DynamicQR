7dcd7342c3aef8ceaa6ad65c22595da7151819cc
ELANGOVAN C <91737914+elango5292@users.noreply.github.com>
2023-10-04 11:47:29 +0530

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,7 +7,7 @@ import React, { useEffect, useState } from 'react';
 
 import useSWR from 'swr'
 
-const fetcher = (...args:any) => fetch(...args:any).then(res => res.json())
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 import {
     Accordion,
